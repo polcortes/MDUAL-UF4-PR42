d01280d0848789af1df1d77f1609c7af3873126a
ErikTamano <72156568+ErikTamanoSantos@users.noreply.github.com>
2023-11-20 19:49:17 +0100

    Add modules

--- app.js
+++ app.js
@@ -1,57 +1,35 @@
 const express = require('express')
-const url = require('url')
-const database = require('./utilsMySQL.js')
-const Obj = require('./utilsMySQL.js')
-const app = express()
-const port = 3000
-const webSockets = require('./utilsWebSockets.js');
+const shadowsObj = require('./utilsShadows.js')
+const webSockets = require('./utilsWebSockets.js')
 ws = new webSockets();
 
-// Crear i configurar l'objecte de la base de dades
-var db = new database();
-db.init({
-  host: 'localhost',
-  port: 3306,
-  user: "root",
-  password: "pwd",
-  database: "Pr42"
-});
+var ws = new webSockets()
+let shadows = new shadowsObj()
 
-// Publicar arxius carpeta ‘public’ 
-app.use(express.static('public'));
+// Start HTTP server
+const app = express()
+const port = process.env.PORT || 8888
 
-// Configurar per rebre dades POST en format JSON
-app.use(express.json());
+// Publish static files from 'public' folder
+app.use(express.static('public'))
 
-// Activar el servidor 
+// Activate HTTP server
 const httpServer = app.listen(port, appListen)
 async function appListen () {
-  await shadows.init('./public/index.html', './public/shadows');
-  console.log(`Example app listening on: http://localhost:${port}`);
+  await shadows.init('./public/index.html', './public/shadows')
+  console.log(`Listening for HTTP queries on: http://localhost:${port}`)
+  console.log(`Development queries on: http://localhost:${port}/index-dev.html`)
 }
 
-
-function connect(protocol, ip, port) {
-    socket = new WebSocket(`${protocol}://${ip}:${port}`)
-    socket.onopen = function(e) {
-        console.log("Socket connected")
-        // Què fer quan un client es connecta?
-    }
-    socket.onmessage = function(event) {
-        let obj = JSON.parse(event.data)
-        // Què fer quan rebem un missatge del servidor?
-    }
-    socket.onerror = function(error) {
-        console.log(`WebSocket error: ${error}`)
-// Què fer quan es produeix un error?
-    }
-    socket.onclose = async function(event) {
-        if (event.wasClean) {
-            console.log(`[close] Connection closed cleanly`)
-        } else {
-            console.log('[close] Connection died')
-        }
-        // Què fer quan un client es desconnecta?
-    }
+// Close connections when process is killed
+process.on('SIGTERM', shutDown);
+process.on('SIGINT', shutDown);
+function shutDown() {
+  console.log('Received kill signal, shutting down gracefully');
+  httpServer.close()
+  ws.end()
+  process.exit(0);
 }
 
+// WebSockets
+ws.init(httpServer, port)
